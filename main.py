03b1146fd3e77489ba612ed8e29486b229a50cf4
RickLiu <rick.liu0121@gmail.com>
2023-07-22 18:21:26 -0400

    Added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#Statement
+
+
+#Variables for the tank
+
+surface_area = 0.32 * 0.26 
+
+#Variables for the tube
+length = [0.2,0.3,0.4,0.6]
+diameter = 0.000794
+sin_beta = 1/150
+
+#Common variables
+density =  1000 #kg/m^3
+P_atm = 101.325 #KPa
+
+#Apply Bernoulli's Equation the first time
+
+
